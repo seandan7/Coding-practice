3313cdc98fd8d752e3555e184205676be9d99c4a
Sean Daniels <Sean Daniels>
2016-03-01 00:57:44 -0800

    Finished assignment

--- day3.js
+++ day3.js
@@ -37,3 +37,73 @@ var test2 = a>b;
 console.log(test1 && test2);
 console.log(test2 || test1);
 */
+
+//ask for age
+/*
+var age = prompt("how old are you?");
+age = parseInt(age);
+
+if(isNaN(age)){
+	alert("Invalid Response");
+}
+
+else if ((age)<18){
+	alert("You are too young");
+}
+else if(age >=18 && age<=50){
+	alert("Welcome to my house!");
+}
+else{
+	alert("You too old playa");
+}*/
+
+
+/*
+Switch Statements
+var condition = "a";
+ 
+switch (condition) {
+case "a":
+    alert("foo");
+    break;//break tells you to go to the end. Take out break, and bar will run too
+case "b":
+    alert("bar");
+    break;
+default:
+alert("wasn't foo or bar");}
+*/
+
+/*using ?
+var loggedin =true;
+var needsLogin = loggedIn ? false:true;
+var needsLogin;
+if(loggedIn){
+	needsLogin = false;
+}
+else{
+	needsLogin = true;
+}
+
+var age = 31;
+var logout = age< 13 ? true: false;
+var logout;
+if (age< 13){
+	logout=true;
+}
+else{
+	logout = false;
+}
+*/
+
+
+var fun = prompt("What do you wanna do?");
+if (fun == null){
+	alert("You have to do something!");
+}
+else if (fun == ""){
+	alert("You have to do something!");
+}
+else{
+	alert(fun);
+	console.log(fun);
+}
