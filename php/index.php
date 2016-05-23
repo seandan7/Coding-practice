c3d23d931ee67e8a63bac2034580ed49cf55587f
Sean <sbd12b@gmail.com>
2016-05-22 23:28:32 -0700

    $_POST practice

--- php/index.php
+++ php/index.php
@@ -1,21 +1,56 @@
-<?php
+<!doctype html>
+<html>
+<head>
+	<title>PHP GET</title>
+	
+	<meta charset="utf-8">
+	<meta http-equiv="Content-type" content="text/html" charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
+	
+	</head>
+	
+<body>
 
-	for($i=1; $i<101; $i++){
-		if($i%15 == 0){
-			echo "fizzbuzz <br />";
+
+<div>
+<?php
+	$names = array("Sean", "Fred", "Bob");
+	
+		if($_POST['submit']){
+			
+			if($_POST['name']){
+				
+				foreach($names as $name) {
+					
+					if($name == $_POST['name']){
+						echo "I know you! Your name is ". $name;
+						
+						$knowYou = true;
+					}
+
+				}
+				
+				if(!$knowYou){
+					echo "I don't know you,". $_POST['name'];
+				}
+				
+			} else {
+			echo "please enter your name";
 		}
-		elseif($i%5 ==0 ){
-			echo "buzz <br />";
-		}
-		elseif($i%3==0){
-			echo "fizz <br />";
-		}
-		else{
-			echo $i." <br />";
-		}
-		
-		
-		
 	}
 	
-?>
+	
+	
+?>	
+
+	
+	<form method="post">
+		<label for="name">Name</label>
+		<input name="name" type="text" />
+		<input name="submit" type="submit" value="submit"/>
+	
+	</form>
+		
+</div>
+</body>
+</html>
