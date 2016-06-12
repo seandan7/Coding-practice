42f7ac4cf9741c25f10222b144157c7afd9a55f1
Sean <sbd12b@gmail.com>
2016-06-12 02:30:49 -0700

    Added class constant code

---
+++ php/php_constants.php
@@ -0,0 +1,27 @@
+<?php 
+
+	//this is an example of class contants
+	class Cars {
+		
+		//Class constants
+		
+			const CAR_01 = 'Ford Ranger';
+			const CAR_02 = 'Ford F150';
+			const CAR_03 = 'Toyota Tacoma';
+			
+		
+	}
+	// class contant syntax
+	// classname::contantname
+	echo "Class contant CAR_01 value :".Cars::CAR_01."<br>";
+	echo "Class contant CAR_02 value :".Cars::CAR_02."<br>";
+	echo "Class contant CAR_03 value :".Cars::CAR_03."<br>";
+	
+?>
+
+	
+	
+	
+	
+	
+	
