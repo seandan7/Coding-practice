f5b78d74c2328733b48c807a705070c6ffdc8eba
Sean <sbd12b@gmail.com>
2016-06-14 16:27:39 -0700

    Destructor, added completed project

--- php/constructor.php
+++ php/constructor.php
@@ -2,36 +2,56 @@
 
 	class Employee {
 		
-		private $first_name;
-		private $last_name;
-		private $id;
+		// Attributes
 		
-		public function __construct($firstName,$lastName,$emp_id){
-			$this->first_name = $firstName;
-			$this->last_name = $lastName;
-			$this->id=$emp_id;
-		}
-		function get_emp(){
-			echo "Name: ".$this->first_name." ".$this->last_name;
-			echo "<br>"."ID Number: ".$this->id."<br><br>";
-		}	
-		function set_employee_info($firstName,$lastName,$emp_id){
-			$this->first_name = $firstName;
-			$this->last_name = $lastName;
-			$this->id = $emp_id;
-	}
+			private $first_name;
+			private $last_name;
+			private $id;
+			
+		// Default Constructor
 		
-		function __destruct(){
-			echo "<p style='font-weight: bold;'>Now destroying previous objects created!</p><br>";
-		}
-	}// end of class Person
-		$emp_01 = new Employee("Sean","Daniels",123);
-		$emp_02 = new Employee("Jim", "Jones",456);
-		$emp_03 = new Employee("Bobby", "Harrison",789);
+			public function __construct($firstName, $lastName, $emp_id){
+				$this->first_name = $firstName;
+				$this->last_name = $lastName;
+				$this->id = $emp_id;
+			}
 		
-		echo "<h1 style='text-align: center;'>Exercise Section 4: The constructor/destructor methods</h1>";
-		echo "<br><h3>List of employees</h3>";
-		$emp_01->get_emp();
-		$emp_02->get_emp();
-		$emp_03->get_emp();
+		// Default destructor
+		
+			function __destruct(){
+				echo "<h3>Now destroying previous objects created...</h3>";
+			}
+		
+		// Methods
+			
+			function set_employee_info($firstName,$lastName,$emp_id){
+				$this->first_name = $firstName;
+				$this->last_name = $lastName;
+				$this->id = $emp_id;
+			}
+		
+			function get_employee_info(){
+				echo "Name: ".$this->first_name." ".$this->last_name;
+				echo "<br>";
+				echo "ID Number: ". $this->id; 
+				echo "<br><br>";
+			}
+		
+	} // end of class Person
+	
+		// Create new objects with values
+		$emp_01 = new Employee("Sean", "D", 123);
+		$emp_02 = new Employee("Jim", "Jones", 456);
+		$emp_03 = new Employee("Sunny", "D", 789);
+		
+		// Display title of exercise project
+		
+			echo "<h1 style='text-align:center;'>Exercise Section 4</h1>";
+			
+		// Display the employees info
+		
+			echo '<h2>List of Employees: </h2>';
+			$emp_01->get_employee_info();
+			$emp_02->get_employee_info();
+			$emp_03->get_employee_info();
 ?>
