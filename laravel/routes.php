2ff1aec1e81ce8b7814de172a70b0008ef088190
Sean <sbd12b@gmail.com>
2016-06-16 02:14:41 -0700

    Laravel brush up. Route practice

---
+++ laravel/routes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('category', function() {
+	return "Category Page Content";
+});
+
+Route::get('category/sf', function() {
+	return "Science Fiction";
+});
+
+Route::get('category/{parameter}', function($parameter) {
+	return "{$parameter} page content";
+});
+
